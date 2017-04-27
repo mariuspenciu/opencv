1ebd1a6896a90aa7ad80a3628fb9323eb0cf0ae7
mariuspenciu <penciumarius@gmail.com>
2017-04-27 19:55:29 +0300

    head detection

--- video.cpp
+++ video.cpp
@@ -15,11 +15,20 @@ int main(void)
 		getchar();
 		return 1;
 	}
+	CascadeClassifier c;
+	c.load("C:\\Users\\Penciu\\Desktop\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_alt2.xml");
+
 	namedWindow("camera");
 
+	vector <Rect> capete;
 	while (1) {
-
+		
 		video >> a;
+		c.detectMultiScale(a, capete);
+
+		for (int i = 0; i < capete.size(); i++)
+			rectangle(a, capete[i], Scalar(255, 0, 0));
+
 		imshow("camera", a);
 
 		if (waitKey(1) == 27)
